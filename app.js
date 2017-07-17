4f4f6a3a49650b75cdf2167ba655e1eeb2fe6346
Titiwut Muktikanantakun <me@iamnut.com>
2017-07-17 10:55:22 +0700

    restyle code and added new feature send log to server

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
-
-var pm2       = require('pm2');
+var pm2 = require('pm2');
+var pmx = require('pmx');
 var SysLogger = require('ain2');
-var logger    = new SysLogger({tag: 'pm2',  facility: 'syslog'});
+
+var config = {};
+
+pmx.initModule({}, function(err, conf){
+    if (err) return console.error(err);
+    config = conf;
+});
+
+var logger = new SysLogger({
+    tag: config.tag,
+    facility: config.facility,
+    hostname: config.hostname,
+    port: parseInt(config.port)
+});
 
 pm2.launchBus(function(err, bus) {
-  bus.on('*', function(event, data){
-    if (event == 'process:event') {
-      logger.warn('app=pm2 target_app=%s target_id=%s restart_count=%s status=%s',
-                  data.process.name,
-                  data.process.pm_id,
-                  data.process.restart_time,
-                  data.event);
-    }
-  });
+    bus.on('*', function(event, data){
+        if (event === 'process:event' && data.event === 'online') {
+            logger.warn('app=pm2 target_app=%s target_id=%s restart_count=%s status=%s',
+                data.process.name,
+                data.process.pm_id,
+                data.process.restart_time,
+                data.event
+            );
+        }
+    });
 
-  bus.on('log:err', function(data) {
-    logger.error('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
-  });
+    bus.on('log:err', function(data) {
+        logger.error('app=%s id=%s |stderr|%s', data.process.name, data.process.pm_id, data.data);
+    });
 
-  bus.on('log:out', function(data) {
-    logger.log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
-  });
+    bus.on('log:out', function(data) {
+        logger.log('app:%s,id:%s|stdout|%s', data.process.name, data.process.pm_id, data.data);
+    });
 });
